3d656c9037b3dc018f035512f24d5fffbca56d63
Your Name <you@example.com>
2024-10-16 11:09:59 +0300

    ✨ (settings.py): add dotenv support to load environment variables for better configuration management ♻️ (settings.py): refactor database settings to use dj_database_url for improved flexibility ✨ (settings.py): add Whitenoise middleware for serving static files in production 📝 (settings.py): update STATICFILES_DIRS to include the static directory for better organization

--- api_backend/settings.py
+++ api_backend/settings.py
@@ -12,8 +12,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
-
 import dj_database_url
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,7 +31,6 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-_n58p&l2!1v4y_%)jx@%47350a
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ["*", 'manychat-api-6217c145bce1.herokuapp.com', 'localhost', '127.0.0.1']
-
 
 # Application definition
 
@@ -52,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'api_backend.urls'
@@ -77,13 +80,11 @@ WSGI_APPLICATION = 'api_backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-# Default: SQLite for local development
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///{}'.format(os.path.join(BASE_DIR, 'db.sqlite3')),
+        conn_max_age=600
+    )
 }
 
 # Password validation
@@ -119,9 +120,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
